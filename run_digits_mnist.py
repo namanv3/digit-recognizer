ff4ec3b9fded134827aecb1a751570bf633b776a
Naman Verma <naman13verma@gmail.com>
2025-11-01 17:03:13 +0530

    split main.py to support multiple use cases

---
+++ run_digits_mnist.py
@@ -0,0 +1,14 @@
+import initializations
+import activation_functions as af
+import cost_functions as cf
+from train_and_test import train_and_test
+
+train_and_test(
+	train_filename="data/digits-mnist/train.csv", 
+	test_filename="data/digits-mnist/test.csv",
+	layer_sizes = [784, 16, 10],
+	activations = [af.LeakyRelU, af.Softmax],
+	cost_function = cf.CrossEntropy,
+	weights_initialization= initializations.he,
+	epochs = 200000
+)
